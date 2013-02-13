1ec40ee1620baa1d1320c544a3ca177779b8fba6
cyberguy42 <dr.justin.smith@gmail.com>
2013-02-13 14:26:29 -0500

    Update fcl.rb

--- fcl.rb
+++ fcl.rb
@@ -9,7 +9,7 @@ class Fcl < Formula
   depends_on 'libccd' => :build
 
   def install
-    system "cmake", ".", *std_cmake_args "-D FCL_USE_SSE:0"
+    system "cmake", ".", *std_cmake_args, "-D FCL_USE_SSE:0"
     system "make install" # if this fails, try separate make/make install steps
   end
 
