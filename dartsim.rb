b16c3c918275c1aefd3c941d28a5b7fcf09d4006
cyberguy42 <dr.justin.smith@gmail.com>
2013-02-13 18:32:43 -0500

    Update dartsim.rb

--- dartsim.rb
+++ dartsim.rb
@@ -9,20 +9,6 @@ class Dartsim < Formula
 		:revision => "ca3042ed7f0f19895eec626edf1b72b93392dd35"
 	version "2.2.1"
 
-	# Standard (on homebrew)
-  depends_on 'cmake' => :build
-  depends_on 'flann' => :build
-  depends_on 'boost' => :build
-  depends_on 'eigen' => :build
-
-	# Non-standard (install through golems)
-  depends_on 'fcl' => :build
-  depends_on 'libccd' => :build
-  depends_on 'assimp3' => :build
-  depends_on 'tinyxml' => :build
-  depends_on 'tinyxml2' => :build
-  depends_on 'gtest' => :build
-
   def install
     system "cmake", ".", *std_cmake_args
     system "make install" # if this fails, try separate make/make install steps
